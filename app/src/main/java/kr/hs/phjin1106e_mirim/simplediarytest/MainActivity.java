fc71c4b8e026aba7648b983239c62f302c4d22e5
ryujeahong <phnin1106@naver.com>
2017-06-01 19:27:43 +0900

    #5 Button Event Programming

--- app/src/main/java/kr/hs/phjin1106e_mirim/simplediarytest/MainActivity.java
+++ app/src/main/java/kr/hs/phjin1106e_mirim/simplediarytest/MainActivity.java
@@ -1,13 +1,17 @@
 package kr.hs.phjin1106e_mirim.simplediarytest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -24,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
         date = (DatePicker)findViewById(R.id.date_pick);
         edit = (EditText)findViewById(R.id.edit);
         but = (Button)findViewById(R.id.but);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileOutputStream fOut = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str=edit.getText().toString();
+                    fOut.write(str.getBytes());
+                    fOut.close();
+                    Toast.makeText(MainActivity.this, "정상적으로 " + fileName + "파일이 저장되었습니다.", Toast.LENGTH_LONG);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
@@ -51,9 +69,16 @@ public class MainActivity extends AppCompatActivity {
             fIn.read(buf);
             diaryStr=new String(buf).trim();
             but.setText("수정 하기");
+            fIn.close();
         } catch (FileNotFoundException e) {
             edit.setText("일기가 존재하지 않습니다.");
             but.setText("새로 저장");
+        } catch (IOException e) {
+
+        }
+
+        try {
+            fIn.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
